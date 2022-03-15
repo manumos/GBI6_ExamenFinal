d20e1a855e24ac202f125b92c3f89bf9ec572183
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-03-14 21:26:08 -0500

    Moscoso Manuela ha resuelto el ejercicio 1.2

---
+++ respaldo_lineacod.R
@@ -0,0 +1,5 @@
+#pregunta 1.1
+getwd() #comprobar directorio actual
+setwd("/Users/Manuela/Documents/GitHub/GBI6_ExamenFinal/")#definir directorio actual
+#pregunta 1.2
+df.mrnae <- read.csv("data/mRNA_expr.csv")
--- results/respaldo_lineacod.R
+++
@@ -1,4 +0,0 @@
-#pregunta 1
-getwd() #comprobar directorio actual
-setwd("/Users/Manuela/Documents/GitHub/GBI6_ExamenFinal/results")#definir directorio actual
-#pregunta2

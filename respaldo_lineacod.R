9e11f530d8df0893bc2114e8e571027a99e98627
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-03-14 23:15:04 -0500

    desarrollo 1.3 y 1.4 en linea de codigo

--- respaldo_lineacod.R
+++ respaldo_lineacod.R
@@ -2,4 +2,16 @@
 getwd() #comprobar directorio actual
 setwd("/Users/Manuela/Documents/GitHub/GBI6_ExamenFinal/")#definir directorio actual
 #pregunta 1.2
-df.mrnae <- read.csv("data/mRNA_expr.csv")
+df<- read.csv("data/mRNA_expr.csv")
+#pregunta 1.3
+install.packages("tidyverse") #instale tydiverse
+library("tidyverse")
+library("tidyr")
+
+#definir funcion en base a un vector que contenga los argumentos
+long_df <- function(a,b,c,d,e) {
+  y=a %>% pivot_longer(!c(b,c),names_to =d, values_to = e)
+  return(y)
+}
+#pregunta 1.4
+df_long <- long_df(df,"bcr_patient_barcode","dataset","gen","expresion_level") %>% select(dataset,gen,expresion_level)

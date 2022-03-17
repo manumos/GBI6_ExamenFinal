6732559d0e521a055c21512f9bc4486726420243
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-03-16 21:10:35 -0500

    Moscoso Manuela ha resuelto los erjicicios 1.6,2.4,2.5 y 2.7

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GBI6_ExamenFinal
 Examen final R
+data= contiene los archivos de los cuales se extraerá la información
+img=imagen para cargar al html
+results=gráficas y metadata de las mismas
+2021I_GBI6_ExamenFinal.RMD=archivo markdown del examen
+2021I_GBI6_ExamenFinal.HTL=archivo HTML del examen 
+respaldo_lineacod =ejericicos resueltos de forma directa
+
+Entorno e historial de R studio
+.Rhistory
+.RData
--- respaldo_lineacod.R
+++ respaldo_lineacod.R
@@ -1,3 +1,11 @@
+#librerias
+library("ggplot2")
+library("dplyr")
+library("tidyr")
+library("tidyverse")
+library("sjmisc")
+library("sjPlot")
+
 #pregunta 1.1
 getwd() #comprobar directorio actual
 setwd("/Users/Manuela/Documents/GitHub/GBI6_ExamenFinal/")#definir directorio actual
@@ -19,10 +27,12 @@ library(dplyr)
 df_long %>%
   group_by(dataset,gen)%>%
   descr(out="viewer",file="results/mRNA_expr_summary.doc")
-#pregunta 1.6
-#interpretacion
+#pregunta 1.6 interpretacion
+
+
 #pregunta 2.1
 library("ggplot2")
+
 tcga_boxplots<- function (x) {
     st<-df_long[df_long$dataset==x,]
     grafica=ggplot(st,aes(dataset,expresion_level)) +
@@ -31,8 +41,6 @@ tcga_boxplots<- function (x) {
     theme(plot.title = element_text(hjust = 0.5, size = 18, face = "bold"))
     return(grafica)
 }
-#prueba
-tcga_boxplots('KIPAN')
 
 #pregunta 2.2
 sets<-c('BRCA','KIPAN','KIRP','LUSC','OV','UCEC')
@@ -47,30 +55,12 @@ ggsave("boxplot3.png",plot=KIRP_plot,device="png",path="results",width=10, heigh
 
 #pregunta 2.4
 genes<- c('GATA3','PTEN','XBP1','ESR1','MUC1','FN1','GAPDH')
-reg_gen_expression<- function(G) {
-  elem<-c(1:7)#lista del 1 al 7
-  pares<-combn(elem,m=2,simplify=F)# combinaciones posibles de 7 elementos
-  df_par<-data.frame(t(sapply(pares,c)))#dataframe de la combinaciones
-  fila<-c(1:21)#total combinaciones
-  for (i in fila){
-    a<-as.numeric(df_par$X1[i])
-    b<-as.numeric(df_par$X2[i])
-    gen1<-factor(G[a])
-    gen2<-factor(G[b])
-    gep<-ggplot(df)+geom_point(aes(gen1,gen2))
-  }
-  gep
+pares<-combn(genes,m=2,simplify=F)
+pares[11]
+ggplot(df,aes(PTEN,GAPDH))+geom_point()+geom_smooth(method='lm', formula= y~x)
+ggsave("regresion11.pdf",device="pdf",path="results",width=20, height=20,units='cm',dpi = 300)
+reg_gen_expression<-function(gen1,gen2){
+  fig=ggplot(df,aes(gen1,gen2))+geom_point()+geom_smooth(method='lm', formula= y~x)
+  return(fig)
 }
-
-
-reg_fun<- function (H){
-  figura=ggplot(df,aes(H[1],H[2]))+geom_point()
-  return(figura)
-}
-#pregunta 2.5
-#pregunta 2.6
-#pregunta 2.7
-
-
-
-
+reg_gen_expression()
